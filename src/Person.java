f9af0b372f65cbbcd2ccf6656ee975b3dc769e83
user_folder <blackbeka319@gmail.com>
2023-07-04 19:45:24 +0600

    I added first class "Person"

--- src/Person.java
+++ src/Person.java
@@ -1,4 +1,14 @@
 public class Person {
-    String Name;
     String Surname;
+    int gpa;
+    int age;
+
+    public Person() {
+    }
+
+    public Person(String surname, int gpa, int age) {
+        Surname = surname;
+        this.gpa = gpa;
+        this.age = age;
+    }
 }
